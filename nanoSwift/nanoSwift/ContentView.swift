cc9ec31adb0e90ef8fc194c3869deb46430960f7
Fernando de Lucas <fernandotiunichristus@gmail.com>
2020-03-12 11:43:22 -0300

    Atualização inicial API de cotação

---
+++ nanoSwift/nanoSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nanoSwift
+//
+//  Created by Fernando de Lucas on 10/03/20.
+//  Copyright © 2020 Fernando de Lucas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        }
+    }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ playgroundNanoswift.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var opit : Int
+var moedaDestino : String?
+
+moedaDestino = readLine()
+
+if let a = moedaDestino{
+    print(a)
+}
